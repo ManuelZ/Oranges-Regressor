d35b45ab8faaa9e0bcc3ad5b172d6a004a541128
Manuel Zanutelli <mclzc89@gmail.com>
2020-06-19 12:48:54 -0500

    Scatter plot in RGB and POC of the HSV colorspace

--- colors_scatter_plot.py
+++ colors_scatter_plot.py
@@ -10,6 +10,10 @@ import vispy.scene
 from vispy.color import ColorArray
 
 
+def scale(arr):
+    return (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+
+
 if __name__ == '__main__':
     
     ap = argparse.ArgumentParser()
@@ -29,20 +33,44 @@ if __name__ == '__main__':
 
     if args['colormap'] == 'rgb':
         b,g,r = cv2.split(im)
-        x,y,z = r,g,b
+        x,y,z = r/255, g/255, b/255
+        color1, color2, color3 = x, y, z
+    
     elif args['colormap'] == 'hsv':
+        # OpenCV uses HSV ranges between (0-180, 0-255, 0-255)
         im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+        
+        # In HSV, H is the theta, S is the radius and V is the altitude
         h,s,v = cv2.split(im)
-        x,y,z = h,s,v
+        # theta, radius, altitude = h,s,v
+        #x = radius * np.cos(np.deg2rad(2 * theta))
+        #y = radius * np.sin(np.deg2rad(2 * theta))
+        #z = altitude
+        
+        radius = np.random.rand(N, 1)
+        theta = np.deg2rad(360 * np.random.rand(N, 1))
+        altitude = np.random.rand(N, 1)
+        
+        x = radius * np.cos(theta)
+        y = radius * np.sin(theta)
+        z = altitude
+
+        color1 = scale(theta)
+        color2 = radius
+        color3 = altitude
+
     else:
         print(f'Only valid colormaps are RGB and HSV')
         sys.exit()
 
+    x = x.reshape((N, 1))
+    y = y.reshape((N, 1))
+    z = z.reshape((N, 1))
 
-    x = x.reshape((N, 1)).astype(np.uint8)
-    y = y.reshape((N, 1)).astype(np.uint8)
-    z = z.reshape((N, 1)).astype(np.uint8)
-    
+    color1 = color1.reshape((N, 1))
+    color2 = color2.reshape((N, 1))
+    color3 = color3.reshape((N, 1))
+
     # Vispy
     Scatter3D = vispy.scene.visuals.create_visual_node(vispy.visuals.MarkersVisual)
 
@@ -50,10 +78,10 @@ if __name__ == '__main__':
     view = canvas.central_widget.add_view()
     # SHIFT + LMB to translate the center point
     view.camera = 'turntable' # 'base', 'arcball', 'turntable', 'panzoom', 'fly'
-    view.camera.distance = 2
+    view.camera.distance = 5
 
-    pos = np.concatenate([x,y,z], axis=1) / 255
-    colors = ColorArray(color_space=args['colormap'], color=(np.concatenate([x,y,z], axis=1) / 255))
+    pos = np.concatenate([x,y,z], axis=1)
+    colors = ColorArray(color=np.concatenate([color1, color2, color3], axis=1), color_space=args['colormap'])
     sizes = 5 * np.ones((N,1))
 
     p1 = Scatter3D(parent=view.scene)
